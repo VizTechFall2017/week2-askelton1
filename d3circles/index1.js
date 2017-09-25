e8033d8ee7cf9735910b720dc56872f8b2625b01
askelton1 <askelton1@github.com>
2017-09-25 17:27:58 -0400

    d3
    final edits

---
+++ d3circles/index1.js
@@ -0,0 +1,267 @@
+console.log("here");
+
+var indexCard = d3.select('body').append('svg')
+    .attr('width', 700)
+    .attr('height', 800);
+
+indexCard.append('line')
+    .attr('x1',400)
+    .attr('y1',100)
+    .attr('x2',400)
+    .attr('y2',700)
+    .attr('stroke','linen')
+    .attr('stroke-width',5);
+
+indexCard.append('line')
+    .attr('x1',300)
+    .attr('y1',100)
+    .attr('x2',300)
+    .attr('y2',700)
+    .attr('stroke','linen')
+    .attr('stroke-width',5);
+
+indexCard.append('line')
+    .attr('x1',200)
+    .attr('y1',100)
+    .attr('x2',200)
+    .attr('y2',700)
+    .attr('stroke','linen')
+    .attr('stroke-width',5);
+
+indexCard.append('line')
+    .attr('x1',500)
+    .attr('y1',100)
+    .attr('x2',500)
+    .attr('y2',700)
+    .attr('stroke','linen')
+    .attr('stroke-width',5);
+
+indexCard.append('line')
+    .attr('x1',600)
+    .attr('y1',100)
+    .attr('x2',600)
+    .attr('y2',700)
+    .attr('stroke','linen')
+    .attr('stroke-width',5);
+
+
+
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',100)
+    .attr('r',50)
+    .attr('fill','salmon');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',200)
+    .attr('r',40)
+    .attr('fill','lightsalmon');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',300)
+    .attr('r',30)
+    .attr('fill','mistyrose');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',400)
+    .attr('r',20)
+    .attr('fill','floralwhite');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',500)
+    .attr('r',30)
+    .attr('fill','oldlace');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',600)
+    .attr('r',40)
+    .attr('fill','peachpuff');
+
+indexCard.append('circle')
+    .attr('cx',400)
+    .attr('cy',700)
+    .attr('r',50)
+    .attr('fill','pink');
+
+
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',100)
+    .attr('r',50)
+    .attr('fill','pink');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',200)
+    .attr('r',40)
+    .attr('fill','peachpuff');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',300)
+    .attr('r',30)
+    .attr('fill','oldlace');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',400)
+    .attr('r',20)
+    .attr('fill','floralwhite');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',500)
+    .attr('r',30)
+    .attr('fill','mistyrose');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',600)
+    .attr('r',40)
+    .attr('fill','lightsalmon');
+
+indexCard.append('circle')
+    .attr('cx',300)
+    .attr('cy',700)
+    .attr('r',50)
+    .attr('fill','salmon');
+
+
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',100)
+    .attr('r',50)
+    .attr('fill','salmon');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',200)
+    .attr('r',40)
+    .attr('fill','lightsalmon');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',300)
+    .attr('r',30)
+    .attr('fill','mistyrose');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',400)
+    .attr('r',20)
+    .attr('fill','floralwhite');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',500)
+    .attr('r',30)
+    .attr('fill','oldlace');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',600)
+    .attr('r',40)
+    .attr('fill','peachpuff');
+
+indexCard.append('circle')
+    .attr('cx',200)
+    .attr('cy',700)
+    .attr('r',50)
+    .attr('fill','pink');
+
+
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',100)
+    .attr('r',50)
+    .attr('fill','pink');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',200)
+    .attr('r',40)
+    .attr('fill','peachpuff');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',300)
+    .attr('r',30)
+    .attr('fill','oldlace');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',400)
+    .attr('r',20)
+    .attr('fill','floralwhite');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',500)
+    .attr('r',30)
+    .attr('fill','mistyrose');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',600)
+    .attr('r',40)
+    .attr('fill','lightsalmon');
+
+indexCard.append('circle')
+    .attr('cx',500)
+    .attr('cy',700)
+    .attr('r',50)
+    .attr('fill','salmon');
+
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',100)
+    .attr('r',50)
+    .attr('fill','salmon');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',200)
+    .attr('r',40)
+    .attr('fill','lightsalmon');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',300)
+    .attr('r',30)
+    .attr('fill','mistyrose');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',400)
+    .attr('r',20)
+    .attr('fill','floralwhite');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',500)
+    .attr('r',30)
+    .attr('fill','oldlace');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',600)
+    .attr('r',40)
+    .attr('fill','peachpuff');
+
+indexCard.append('circle')
+    .attr('cx',600)
+    .attr('cy',700)
+    .attr('r',50)
+    .attr('fill','pink');
+
+
